cd7b56d1b301d021eab1619fb5d6784801c481b3
Callum Bondy <callum.bondy@gmail.com>
2025-04-03 14:38:14 -0500

    added new effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from 'react';
+import { useRef, useEffect } from 'react';
+
 function OptionOne() {
   return (
     <div
@@ -298,8 +301,49 @@ function OptionFour() {
   );
 }
 
+function OptionFive() {
+  const container = useRef(null)
+  const stickyMask = useRef(null)
+
+  const initialMaskSize = 0.8
+  const targetMaskSize = 50
+
+  const easing = 0.15
+  let easedScrollProgress = 0
+
+  useEffect(() => {
+    requestAnimationFrame(animate)
+  },[])
+
+  const animate = () => {
+    const maskSizeProgress = targetMaskSize * getScrollProgress()
+    stickyMask.current.style.webkitMaskSize = (initialMaskSize + maskSizeProgress) * 100 + '%'
+    requestAnimationFrame(animate)
+  }
+
+  const getScrollProgress = () => {
+    const scrollProgress = stickyMask.current.offsetTop / (container.current.getBoundingClientRect().height - window.innerHeight)
+    const delta = scrollProgress - easedScrollProgress
+    easedScrollProgress += delta * easing
+    return easedScrollProgress
+    
+  }
+
+  return(
+    <main className="main">
+    <div ref={container} className="container">
+      <div ref={stickyMask} className="stickyMask">
+        <video autoPlay muted loop>
+          <source src="test.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </div>
+    </main>
+  );
+}
+
 function App() {
-  return <OptionFour></OptionFour>;
+  return <OptionFive></OptionFive>;
 }
 
 export default App;
